d8978468b6bad181043d8ef5cc84f6d0932c0f7a
Steffen Christgau <christgau@zib.de>
2020-04-01 17:23:38 +0200

    refactor: track address and size of allocations

--- hppalloc.c
+++ hppalloc.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef DEBUG
 #define debug_print(...) do {\
@@ -12,34 +13,111 @@
 #define debug_print(...)
 #endif
 
-static bool initialized = false;
+#define ALLOCATION_GROWTH 32
+
+typedef struct {
+	void* addr;
+	size_t size;
+} allocation_t;
+
+static struct {
+	allocation_t *items;
+	size_t count;
+	size_t capacity;
+} allocations = { NULL, 0, 0 };
+
+
+static bool hpp_grow_allocations()
+{
+	size_t nc = allocations.capacity + ALLOCATION_GROWTH;
+
+	/* reallocarray is not available on CentOS 7 */
+	void* ni = realloc(allocations.items,  nc * sizeof(*allocations.items));
+	if (!ni) {
+		debug_print("fatal: could not increase intenal memory\n");
+		return false;
+	}
+
+	allocations.items = ni;
+	allocations.capacity = nc;
+
+	return true;
+}
+
+
+static allocation_t* hpp_reserve_allocation(void)
+{
+	if (allocations.count == allocations.capacity) {
+		hpp_grow_allocations();
+	}
+	return &allocations.items[allocations.count++];
+}
+
+
+static allocation_t* hpp_get_allocation(const void* addr)
+{
+	for (size_t i = 0; i < allocations.count; i++) {
+		if (allocations.items[i].addr == addr) {
+			return &allocations.items[i];
+		}
+	}
+
+	return NULL;
+}
+
+
+static bool is_initialized = false;
 
 static void hpp_init(void)
 {
-	if (initialized) {
+	if (is_initialized) {
 		return;
 	}
 
-	initialized = true;
+	is_initialized = true;
+	hpp_grow_allocations();
+
 	debug_print("initialized\n");
 }
 
+/* actual allocation/deallocation */
+
 void* hpp_alloc(size_t n, size_t elem_size)
 {
-	if (!initialized) {
+	if (!is_initialized) {
 		hpp_init();
 	}
 
 	/* no overflow checks as in calloc here */
-	size_t alloc_size = n * elem_size;
-	void* retval = malloc(alloc_size);
-	debug_print("allocated %zu * %zu Bytes = %zu Bytes @ %p\n", n, elem_size, alloc_size, retval);
+	allocation_t *a = hpp_reserve_allocation();
+	if (!a) {
+		return NULL;
+	}
 
-	return retval;
+	a->size = n * elem_size;
+	a->addr = malloc(a->size);
+	debug_print("allocated %zu * %zu Bytes = %zu Bytes @ %p\n", n, elem_size, a->size, a->addr);
+
+	return a->addr;
 }
+
 
 void hpp_free(void *ptr)
 {
-	debug_print("free %p\n", ptr);
+	allocation_t* a = hpp_get_allocation(ptr);
+	if (!a) {
+		debug_print("unknown allocation at %p\n", a->addr);
+		return;
+	}
+
+	if (a->size == SIZE_MAX) {
+		debug_print("double free at %p detected\n", ptr);
+		return;
+	}
+
+	debug_print("free %zu byte at %p\n", a->size, a->addr);
 	free(ptr);
+
+	a->addr = NULL;
+	a->size = SIZE_MAX;
 }
